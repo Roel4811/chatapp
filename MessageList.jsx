99e6f47e58485437cbfba7e55c3afe5016d2b1ad
Roel de Jong <Roel4811@hotmail.com>
2016-12-26 01:46:58 +0100

    add message input without page reload

--- MessageList.jsx
+++ MessageList.jsx
@@ -13,12 +13,34 @@ MessageList = React.createClass({
     });
   },
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let message = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Meteor.call("addMessage", message);
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  renderForm() {
+    return <form onSubmit={this.handleSubmit} >
+      <input
+        type="text"
+        ref="textInput"
+        name="message"
+        placeholder="Enter message..." />
+      </form>
+  },
+
   render() {
     return (
       <div className="container">
         <header>
           <h2>Messages</h2>
         </header>
+
+        {this.renderForm()}
 
         <ul>
           {this.renderMessages()}
--- demo.jsx
+++ demo.jsx
@@ -1,5 +1,16 @@
 Messages = new Mongo.Collection("Messages", {});
 
+Meteor.methods({
+  addMessage(text) {
+    let message = {
+      time: new Date(),
+      text: text
+    };
+
+    Messages.insert(message);
+  }
+});
+
 if (Meteor.isClient) {
   Meteor.startup(function () {
     ReactDOM.render(<MessageList />,
